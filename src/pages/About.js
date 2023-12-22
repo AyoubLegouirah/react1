b7982477f4f5652d1ac1049b99dbbe4822fbac98
AyoubLegouirah <ayoubgrand@outlook.fr>
2023-12-22 14:13:17 +0100

    react

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation  /> 
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorem nulla perspiciatis tempore eum ipsam, ut temporibus quasi rerum, veritatis saepe suscipit eaque sapiente quisquam, officiis id at? Id, reprehenderit!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus assumenda ratione molestias fuga vel ipsa voluptates illum earum ullam amet natus quod placeat, minima impedit ducimus, architecto saepe repellat et?</p>
+        </div>
+    );
+};
+
+export default About;
